e4cedacbc132330aaa3dc18f279a55ef176fa01b
LordNorbi <roberthaecker@web.de>
2018-05-02 10:23:02 +0200

    generator update for new blocks

--- forTaskGen/setgenerator.py
+++ forTaskGen/setgenerator.py
@@ -1,38 +1,41 @@
-from taskset import TaskSet
-from task import Task
-from blocks import *
+from taskgen.taskset import TaskSet
+from taskgen.task import Task
+from taskgen.blocks import *
 
 class SetGenerator(TaskSet):
-    numberOfVariants = 2                                #50
-    priorities = {"priority" : [16,64]}                 #[8,16,32,64,128]
-    periods = {"period" : [0,5]}                        #[0,5,10]
+    
+    criticaltime = {"criticaltime" : [1000,2000,3000]}
+    deadline = {"deadline" : [3000,6000,9000]}
+    numberofjobs = {"numberofjobs" : [1,8,16]}
+    numberOfVariants = 50
+    periods = {"period" : [0,500,10000]}
+    priorities = {"priority" : [8,32,128]}
+    
+    #just commt out if it'S to much or reduce numberOfVariants
+    # Hey           =     1
+    # Pi            =    10
+    # cond_42       =    10
+    # cond_mod      =    10
+    # linpack       =    10
+    # ---------------------
+    # 1^10^10^10^10 = 10000
+    
+    #for even more Tasksets replace HighRandom() with Variants() then not only 1 Value is used but all possible values (at period and at priority possible)
+    #or add even more Tasks
     
     def returnrobert(self):
         
-        #just commt out if it'S to much or reduce numberOfVariants
-        # Hey           =     1
-        # Pi            =    10
-        # cond_42       =    10
-        # cond_mod      =    10
-        # linpack       =    10
-        # ---------------------
-        # 1^10^10^10^10 = 10000
-        
-        #for even more Tasksets replace HighRandom() with Variants() then not only 1 Value is used but all possible values (at period and at priority possible)
-        #or add even more Tasks
-        
         set = TaskSet([])
-        numberOfVariants = 10
         
-        task01 = Task(hey.HelloWorld, period.HighRandom(), priority.HighRandom())
+        task01 = Task(hey.Value(1), period.HighRandom(), priority.HighRandom())
         set.append(task01)
-        task02 = Task(pi.Variants(numberOfVariants), period.HighRandom(), priority.HighRandom())
+        task02 = Task(pi.Variants(self.numberOfVariants), period.HighRandom(), priority.HighRandom())
         set.append(task02)
-        task03 = Task(cond_42.Variants(numberOfVariants), period.HighRandom(), priority.HighRandom())
+        task03 = Task(cond_42.Variants(self.numberOfVariants), period.HighRandom(), priority.HighRandom())
         set.append(task03)
-        task04 = Task(cond_mod.Variants(numberOfVariants), period.HighRandom(), priority.HighRandom())
+        task04 = Task(cond_mod.Variants(self.numberOfVariants), period.HighRandom(), priority.HighRandom())
         set.append(task04)
-        task05 = Task(linpack.Variants(numberOfVariants), period.HighRandom(), priority.HighRandom())
+        task05 = Task(linpack.Variants(self.numberOfVariants), period.HighRandom(), priority.HighRandom())
         set.append(task05)
         
         return set
@@ -41,13 +44,10 @@ class SetGenerator(TaskSet):
     def size2(self):
         
         set = TaskSet([])
-        numberOfVariants = self.numberOfVariants
-        priorities = self.priorities
-        periods = self.periods
         
-        task02 = Task(pi.Variants(numberOfVariants), periods, priorities)
+        task02 = Task(pi.Variants(self.numberOfVariants),self.criticaltime, self.deadline, self.periods, self.priorities, self.numberofjobs,)
         set.append(task02)
-        task03 = Task(cond_42.Variants(numberOfVariants), periods, priorities)
+        task03 = Task(cond_42.Variants(self.numberOfVariants),self.criticaltime, self.deadline, self.periods, self.priorities, self.numberofjobs)
         set.append(task03)
         
         return set
@@ -56,15 +56,12 @@ class SetGenerator(TaskSet):
     def size3(self):
         
         set = TaskSet([])
-        numberOfVariants = self.numberOfVariants
-        priorities = self.priorities
-        periods = self.periods
         
-        task02 = Task(pi.Variants(numberOfVariants), periods, priorities)
+        task02 = Task(pi.Variants(self.numberOfVariants),self.criticaltime, self.deadline, self.periods, self.priorities, self.numberofjobs)
         set.append(task02)
-        task03 = Task(cond_42.Variants(numberOfVariants), periods, priorities)
+        task03 = Task(cond_42.Variants(self.numberOfVariants),self.criticaltime, self.deadline, self.periods, self.priorities, self.numberofjobs)
         set.append(task03)
-        task04 = Task(cond_mod.Variants(numberOfVariants), periods, priorities)
+        task04 = Task(cond_mod.Variants(self.numberOfVariants),self.criticaltime, self.deadline, self.periods, self.priorities, self.numberofjobs)
         set.append(task04)
         
         return set
@@ -73,17 +70,17 @@ class SetGenerator(TaskSet):
     def size4(self):
         
         set = TaskSet([])
-        numberOfVariants = self.numberOfVariants
-        priorities = self.priorities
-        periods = self.periods
+        numberOfVariants = 50
+        priorities = {"priority" : [8,16,32,64,128]}
+        periods = {"period" : [0,500,10000]}
         
-        task02 = Task(pi.Variants(numberOfVariants), periods, priorities)
+        task02 = Task(pi.Variants(self.numberOfVariants),self.criticaltime, self.deadline, self.periods, self.priorities, self.numberofjobs)
         set.append(task02)
-        task03 = Task(cond_42.Variants(numberOfVariants), periods, priorities)
+        task03 = Task(cond_42.Variants(self.numberOfVariants),self.criticaltime, self.deadline, self.periods, self.priorities, self.numberofjobs)
         set.append(task03)
-        task04 = Task(cond_mod.Variants(numberOfVariants), periods, priorities)
+        task04 = Task(cond_mod.Variants(self.numberOfVariants),self.criticaltime, self.deadline, self.periods, self.priorities, self.numberofjobs)
         set.append(task04)
-        task05 = Task(linpack.Variants(numberOfVariants), periods, priorities)
+        task05 = Task(linpack.Variants(self.numberOfVariants),self.criticaltime, self.deadline, self.periods, self.priorities, self.numberofjobs)
         set.append(task05)
         
         return set
@@ -92,19 +89,16 @@ class SetGenerator(TaskSet):
     def size5(self):
         
         set = TaskSet([])
-        numberOfVariants = self.numberOfVariants
-        priorities = self.priorities
-        periods = self.periods
         
-        task02 = Task(pi.Variants(numberOfVariants), periods, priorities)
+        task02 = Task(pi.Variants(self.numberOfVariants),self.criticaltime, self.deadline, self.periods, self.priorities, self.numberofjobs)
         set.append(task02)
-        task03 = Task(cond_42.Variants(numberOfVariants), periods, priorities)
+        task03 = Task(cond_42.Variants(self.numberOfVariants),self.criticaltime, self.deadline, self.periods, self.priorities, self.numberofjobs)
         set.append(task03)
-        task04 = Task(cond_mod.Variants(numberOfVariants), periods, priorities)
+        task04 = Task(cond_mod.Variants(self.numberOfVariants),self.criticaltime, self.deadline, self.periods, self.priorities, self.numberofjobs)
         set.append(task04)
-        task05 = Task(linpack.Variants(numberOfVariants), periods, priorities)
+        task05 = Task(linpack.Variants(self.numberOfVariants),self.criticaltime, self.deadline, self.periods, self.priorities, self.numberofjobs)
         set.append(task05)
-        task06 = Task(cond_mod.Variants(numberOfVariants), periods, priorities)
+        task06 = Task(cond_mod.Variants(self.numberOfVariants),self.criticaltime, self.deadline, self.periods, self.priorities, self.numberofjobs)
         set.append(task06)
         
         return set
@@ -112,36 +106,21 @@ class SetGenerator(TaskSet):
     def size6(self):
         
         set = TaskSet([])
-        numberOfVariants = self.numberOfVariants
-        priorities = self.priorities
-        periods = self.periods
         
-        task02 = Task(pi.Variants(numberOfVariants), periods, priorities)
+        task02 = Task(pi.Variants(self.numberOfVariants),self.criticaltime, self.deadline, self.periods, self.priorities, self.numberofjobs)
         set.append(task02)
-        task03 = Task(cond_42.Variants(numberOfVariants), periods, priorities)
+        task03 = Task(cond_42.Variants(self.numberOfVariants),self.criticaltime, self.deadline, self.periods, self.priorities, self.numberofjobs)
         set.append(task03)
-        task04 = Task(cond_mod.Variants(numberOfVariants), periods, priorities)
+        task04 = Task(cond_mod.Variants(self.numberOfVariants),self.criticaltime, self.deadline, self.periods, self.priorities, self.numberofjobs)
         set.append(task04)
-        task05 = Task(linpack.Variants(numberOfVariants), periods, priorities)
+        task05 = Task(linpack.Variants(self.numberOfVariants),self.criticaltime, self.deadline, self.periods, self.priorities, self.numberofjobs)
         set.append(task05)
-        task06 = Task(cond_mod.Variants(numberOfVariants), periods, priorities)
+        task06 = Task(cond_mod.Variants(self.numberOfVariants),self.criticaltime, self.deadline, self.periods, self.priorities, self.numberofjobs)
         set.append(task06)
-        task07 = Task(linpack.Variants(numberOfVariants), periods, priorities)
+        task07 = Task(linpack.Variants(self.numberOfVariants),self.criticaltime, self.deadline, self.periods, self.priorities, self.numberofjobs)
         set.append(task07)
         
         return set
-    
-    def getPossibilities(self, taskset):
-        
-        setgen = taskset.variants()
-        
-        cSet = setgen.__next__()
-        #print(cSet)
-        i = 1
-        try:
-            while cSet!=None:
-                cSet = setgen.__next__()
-                i = i + 1
-        except:
-            return i
-        return i
+
+
+
